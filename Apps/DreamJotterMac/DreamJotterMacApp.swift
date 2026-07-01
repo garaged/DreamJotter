3a6f15ce99e1089b0d83653144078fc66d11f896
GaRaGeD <garaged@gmail.com>
2026-07-01 08:16:37 -0600

    Activate macOS app for keyboard focus

--- Apps/DreamJotterMac/DreamJotterMacApp.swift
+++ Apps/DreamJotterMac/DreamJotterMacApp.swift
@@ -1,11 +1,21 @@
+import AppKit
 import SwiftUI
 
 @main
 struct DreamJotterMacApp: App {
+    @NSApplicationDelegateAdaptor(DreamJotterMacApplicationDelegate.self) private var appDelegate
+
     var body: some Scene {
         WindowGroup {
             AppRootView()
         }
         .windowStyle(.titleBar)
+    }
+}
+
+final class DreamJotterMacApplicationDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.regular)
+        NSApp.activate(ignoringOtherApps: true)
     }
 }
