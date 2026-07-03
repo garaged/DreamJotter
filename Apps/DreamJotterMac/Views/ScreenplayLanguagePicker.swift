e1fb3ecdbb592b96aefe627ef028e76c2f574ddc
garaged <garaged@gmail.com>
2026-07-02 20:27:54 -0600

    Avoid duplicate picker declaration

--- Apps/DreamJotterMac/Views/ScreenplayLanguagePicker.swift
+++ Apps/DreamJotterMac/Views/ScreenplayLanguagePicker.swift
@@ -1,18 +1,1 @@
-import DreamJotterCore
 import SwiftUI
-
-struct ScreenplayLanguagePicker: View {
-    @Binding var document: ProjectDocumentViewModel
-
-    var body: some View {
-        Picker("Screenplay Language", selection: Binding(
-            get: { document.screenplayLanguage },
-            set: { document.setScreenplayLanguage($0) }
-        )) {
-            Text("Automatic").tag(ScreenplayLanguageProfile.automatic)
-            Text("English").tag(ScreenplayLanguageProfile.english)
-            Text("Spanish (Latin America)").tag(ScreenplayLanguageProfile.spanishLatinAmerica)
-        }
-        .frame(width: 210)
-    }
-}
