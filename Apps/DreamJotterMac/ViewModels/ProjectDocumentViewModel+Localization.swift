5d4fd296bb58f9b0e72350e9824a7a64a3c98709
garaged <garaged@gmail.com>
2026-07-02 20:10:51 -0600

    Apply project language during editor reparsing

--- Apps/DreamJotterMac/ViewModels/ProjectDocumentViewModel+Localization.swift
+++ Apps/DreamJotterMac/ViewModels/ProjectDocumentViewModel+Localization.swift
@@ -27,11 +27,48 @@ extension ProjectDocumentViewModel {
             story: projectWithSetting.story,
             pro: projectWithSetting.pro
         )
-        self = ProjectDocumentViewModel(
-            project: updated,
-            packageURL: packageURL,
-            scriptText: scriptText,
-            isDirty: true
-        )
+        self = ScreenplayParsingContext.$language.withValue(language) {
+            ProjectDocumentViewModel(
+                project: updated,
+                packageURL: packageURL,
+                scriptText: scriptText,
+                isDirty: true
+            )
+        }
+    }
+
+    mutating func updateScriptTextRespectingLanguage(_ text: String) {
+        let language = screenplayLanguage
+        ScreenplayParsingContext.$language.withValue(language) {
+            updateScriptText(text)
+        }
+    }
+
+    mutating func refreshParseRespectingLanguage(now: Date = Date()) {
+        let language = screenplayLanguage
+        ScreenplayParsingContext.$language.withValue(language) {
+            refreshParse(now: now)
+        }
+    }
+
+    mutating func acceptEditorSuggestionRespectingLanguage(_ suggestion: EditorSuggestion) {
+        let language = screenplayLanguage
+        ScreenplayParsingContext.$language.withValue(language) {
+            acceptEditorSuggestion(suggestion)
+        }
+    }
+
+    mutating func performSmartEnterRespectingLanguage(at cursorLocation: Int) {
+        let language = screenplayLanguage
+        ScreenplayParsingContext.$language.withValue(language) {
+            performSmartEnter(at: cursorLocation)
+        }
+    }
+
+    mutating func performTabCycleRespectingLanguage(at cursorLocation: Int) {
+        let language = screenplayLanguage
+        ScreenplayParsingContext.$language.withValue(language) {
+            performTabCycle(at: cursorLocation)
+        }
     }
 }
