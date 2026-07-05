66765ccdec0a82013a639e898c06d0d1698fb6e8
garaged <garaged@gmail.com>
2026-07-04 18:17:45 -0600

    Restore language-aware editor mutations

--- Apps/DreamJotterMac/Views/ScriptEditorView.swift
+++ Apps/DreamJotterMac/Views/ScriptEditorView.swift
@@ -352,6 +352,32 @@ extension ProjectDocumentViewModel {
             snapshots: configured.snapshots, exportPresets: configured.exportPresets,
             story: configured.story, pro: configured.pro
         )
-        replaceProject(updated, updateScriptText: false)
+        self = ScreenplayParsingContext.$language.withValue(language) {
+            ProjectDocumentViewModel(project: updated, packageURL: packageURL, scriptText: scriptText, isDirty: true)
+        }
+    }
+
+    mutating func updateScriptTextRespectingLanguage(_ text: String) {
+        ScreenplayParsingContext.$language.withValue(screenplayLanguage) { updateScriptText(text) }
+    }
+
+    mutating func refreshParseRespectingLanguage(now: Date = Date()) {
+        ScreenplayParsingContext.$language.withValue(screenplayLanguage) { refreshParse(now: now) }
+    }
+
+    mutating func acceptEditorSuggestionRespectingLanguage(_ suggestion: EditorSuggestion) {
+        ScreenplayParsingContext.$language.withValue(screenplayLanguage) { acceptEditorSuggestion(suggestion) }
+    }
+
+    mutating func performSmartEnterRespectingLanguage(at cursorLocation: Int) {
+        ScreenplayParsingContext.$language.withValue(screenplayLanguage) { performSmartEnter(at: cursorLocation) }
+    }
+
+    mutating func performTabCycleRespectingLanguage(at cursorLocation: Int) {
+        ScreenplayParsingContext.$language.withValue(screenplayLanguage) { performTabCycle(at: cursorLocation) }
+    }
+
+    mutating func saveRespectingLanguage(to packageURL: URL, now: Date = Date()) throws {
+        try ScreenplayParsingContext.$language.withValue(screenplayLanguage) { try save(to: packageURL, now: now) }
     }
 }
