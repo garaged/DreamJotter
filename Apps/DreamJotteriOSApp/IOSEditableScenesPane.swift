4eac3741b27cf5351903b38b0eef52de43a2fbb8
garaged <garaged@gmail.com>
2026-07-05 17:24:57 -0600

    Select scene cards for editing

--- Apps/DreamJotteriOSApp/IOSEditableScenesPane.swift
+++ Apps/DreamJotteriOSApp/IOSEditableScenesPane.swift
@@ -4,8 +4,19 @@ import SwiftUI
 struct IOSEditableScenesPane: View {
     @Binding var project: DreamJotterProject
     let commitProjectChange: (DreamJotterProject) -> Void
+    @State private var selectedCard: SceneCard?
 
     var body: some View {
-        IOSScenesPane(project: project)
+        VStack {
+            IOSScenesPane(project: project)
+            if let firstCard = SceneWorkflow.cards(in: project).first {
+                Button("Edit First Scene") {
+                    selectedCard = firstCard
+                }
+            }
+        }
+        .sheet(item: $selectedCard) { card in
+            IOSSceneCardEditorSheet(card: card)
+        }
     }
 }
