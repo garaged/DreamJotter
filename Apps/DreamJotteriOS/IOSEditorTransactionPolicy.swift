b30e3e30380dc25eaf3cc244eeabda7de1569a5a
garaged <garaged@gmail.com>
2026-07-05 05:03:04 -0600

    Define editor transaction boundaries

---
+++ Apps/DreamJotteriOS/IOSEditorTransactionPolicy.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+public struct IOSEditorTransaction: Equatable, Sendable {
+    public let actionName: String
+    public let groupsWithTyping: Bool
+
+    public init(actionName: String, groupsWithTyping: Bool) {
+        self.actionName = actionName
+        self.groupsWithTyping = groupsWithTyping
+    }
+}
+
+public enum IOSEditorTransactionPolicy {
+    public static func transaction(for kind: IOSEditorMutationKind) -> IOSEditorTransaction {
+        switch kind {
+        case .typing:
+            return IOSEditorTransaction(actionName: "Typing", groupsWithTyping: true)
+        case .paste:
+            return IOSEditorTransaction(actionName: "Paste", groupsWithTyping: false)
+        case .smartEnter:
+            return IOSEditorTransaction(actionName: "Smart Enter", groupsWithTyping: false)
+        case .elementFormatting:
+            return IOSEditorTransaction(actionName: "Format Element", groupsWithTyping: false)
+        case .suggestionAcceptance:
+            return IOSEditorTransaction(actionName: "Accept Suggestion", groupsWithTyping: false)
+        case .undo:
+            return IOSEditorTransaction(actionName: "Undo", groupsWithTyping: false)
+        case .redo:
+            return IOSEditorTransaction(actionName: "Redo", groupsWithTyping: false)
+        }
+    }
+}
